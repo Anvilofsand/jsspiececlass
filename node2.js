8d0977793b7e08f1356cc2a0bfad436605d744de
Anvilofsand <121681433+Anvilofsand@users.noreply.github.com>
2024-05-29 11:47:13 -0400

    Create node2.js

---
+++ node2.js
@@ -0,0 +1,11 @@
+let animals = ["Dog", "Cat", "Elephant"]; 
+let secondAnimal = animals[1] = "Cat";
+animals[2] = "Lion"; 
+console.log(animals);
+
+let scores = [85,90,78];
+let firstscore = scores [0]
+scores.push(88);
+console.log (scores)
+console.log (firstscore)
+
